88a81ee4d42af62118d84e4cf92a990be4635ec4
Ashish kumar <ashishkumar0842a@gmail.com>
2023-11-14 13:14:09 +0530

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+
+function Home(){
+  return(
+    <h1>Home</h1>
+  )
+}
+
+function About(){
+  return(
+    <h1>About</h1>
+  )
+}
+
+function Account(){
+  return(
+    <h1>Account Page</h1>
+  )
+}
+function Setting(){
+  return(
+    <h1>Setting Page</h1>
+  )
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={ <Home />} />
+      <Route path='/about' element={ <About />} />
+      <Route path='/account'>
+      <Route path='page' element={ <Account />} />
+      <Route path='setting' element={ <Setting />} />
+      </Route>
+    </Routes>
+   </BrowserRouter>
   );
 }
 
